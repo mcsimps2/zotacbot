9253c431ea0b3e82c1e506a9bcb92d893043651b
Matthew Simpson <mcsimps2@gmail.com>
2020-11-21 23:26:54 -0500

    Add linux support

--- zotacbot/util.py
+++ zotacbot/util.py
@@ -14,8 +14,15 @@ def _alert_win(msg):
     winsound.Beep(2000, 1000)
 
 
+def _alert_linux(msg):
+    print(f"ALERT: {msg}")
+    print("\a")
+
+
 def alert(msg):
     if psutil.WINDOWS:
         return _alert_win(msg)
-    else:
+    elif psutil.MACOS:
         return _alert_mac(msg)
+    else:
+        return _alert_linux(msg)
